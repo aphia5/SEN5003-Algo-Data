db988d239cb6c3862614539d8ff2738a5e6ea140
aphia5 <aasszaphia@gmail.com>
2026-01-11 01:32:00 +0000

    Add binary search method in BinarySearchClass
    Implement binary search method for string search.

---
+++ BinarySearchClass.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class BinarySearchClass {
+
+
+    public static String binarySearchFTD(ArrayList<String> list, String FromToDate, String FromTo, String From, String Date) {
+        int start = 0;
+        int end = list.size() - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            String midVal = list.get(mid);
+
+            if (FromToDate.compareTo(midVal) == 0) {
+                System.out.println(midVal + " is avaliable");
+                return midVal;
+            }
+            if (FromToDate.compareTo(midVal) < 0) {
+                end = mid - 1;
+            }
+            if (FromToDate.compareTo(midVal) > 0) {
+                start = mid + 1;
+            }
+        }
+        System.out.println(FromToDate + " is not avaliable");
+        LinearSerchClass.linearSearchFT(list, FromTo, From, Date);
+        return null;
+    }
+
+}
