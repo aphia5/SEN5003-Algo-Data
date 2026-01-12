8ee2d3ce44d2fd12fefef0b638b2a0cfde0086e3
aphia5 <aasszaphia@gmail.com>
2026-01-12 00:24:42 +0000

    Refactor MergeSortClass for clarity and performance
    Refactor MergeSortClass for improved readability and efficiency.

--- MergeSortClass.java
+++ MergeSortClass.java
@@ -1,20 +1,17 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MergeSortClass {
     public static ArrayList<String> mergeSort(ArrayList<String> arraylist){
 
-        //Make Variables
+        // Make Variables
         int length = arraylist.size();
-        int mid = arraylist.size()/2;
+        int mid = length / 2;
 
-        //Check For Valid Length
-        if(length <=1){
-            return arraylist;
-        }
+        // Check For Valid Length
+        if(length <= 1) return arraylist;
 
-        ArrayList<String> leftHalf = new ArrayList<>(arraylist.subList(0, mid)); //SOURCE ARRAY, START, END
-        ArrayList<String> rightHalf = new ArrayList<>(arraylist.subList(mid, length)); //SOURCE ARRAY, START, END
+        ArrayList<String> leftHalf = new ArrayList<>(arraylist.subList(0, mid)); // SOURCE ARRAY, START, END
+        ArrayList<String> rightHalf = new ArrayList<>(arraylist.subList(mid, length)); // SOURCE ARRAY, START, END
 
         leftHalf = mergeSort(leftHalf);
         rightHalf = mergeSort(rightHalf);
@@ -23,33 +20,20 @@ public class MergeSortClass {
     }
 
     public static ArrayList<String> merge(ArrayList<String> left, ArrayList<String> right){
-
         ArrayList<String> merged = new ArrayList<>();
-        int i = 0, j = 0; //LEFT, RIGHT, MERGED ITERATOR
+        int i = 0, j = 0; // LEFT, RIGHT ITERATOR
 
-        int leftLength = left.size();
-        int rightLength = right.size();
-
-        while (i < leftLength && j < rightLength){
+        while (i < left.size() && j < right.size()){
             if (left.get(i).compareTo(right.get(j)) <= 0){
                 merged.add(left.get(i));
                 i++;
-            }
-            else{
+            } else {
                 merged.add(right.get(j));
                 j++;
             }
         }
-
-        while (i < leftLength){
-            merged.add(left.get(i));
-            i++;
-        }
-
-        while (j < rightLength){
-            merged.add(right.get(j));
-            j++;
-        }
+        while (i < left.size()) merged.add(left.get(i++));
+        while (j < right.size()) merged.add(right.get(j++));
 
         return merged;
     }
